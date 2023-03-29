0bc00b4923108b408af465e846c6e1afe35ebb56
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2023-03-29 14:13:54 +0200

    chore: add better example (#11)

--- example_test.go
+++ example_test.go
@@ -1,71 +1,20 @@
 package batchiterator_test
 
 import (
-	"context"
-	"database/sql"
-	"errors"
-	"log"
+	"strconv"
 	"testing"
-
-	batchiterator "github.com/Eun/go-batch-iterator"
-	_ "github.com/glebarez/go-sqlite"
 )
 
-func ExampleIterator() {
-	db, err := sql.Open("sqlite", ":memory:")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	defer db.Close()
-
-	_, err = db.Exec(`
-CREATE TABLE users (name TEXT);
-INSERT INTO users (name) VALUES ("Alice"), ("Bob"), ("Charlie"), ("George"), ("Gerald"), ("Joe"), ("John"), ("Zoe");
-`)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	maxRowsPerQuery := 3
-	offset := 0
-	iter := batchiterator.Iterator[string]{
-		NextBatchFunc: func(ctx context.Context) (items []string, hasMoreItems bool, err error) {
-			rows, err := db.QueryContext(ctx, "SELECT name FROM users LIMIT ? OFFSET ?", maxRowsPerQuery, offset)
-			if err != nil {
-				if errors.Is(err, sql.ErrNoRows) {
-					return nil, false, nil
-				}
-				return nil, false, err
-			}
-			defer rows.Close()
-
-			var names []string
-			for rows.Next() {
-				var name string
-				if err := rows.Scan(&name); err != nil {
-					return nil, false, err
-				}
-				names = append(names, name)
-			}
-			if err := rows.Err(); err != nil {
-				return nil, false, err
-			}
-			offset += maxRowsPerQuery
-			moreRowsAvailable := len(names) == maxRowsPerQuery
-			return names, moreRowsAvailable, nil
-		},
-	}
-	for iter.Next(context.Background()) {
-		log.Print(*iter.Value())
-	}
-	if err := iter.Error(); err != nil {
-		log.Print(err)
-		return
-	}
-}
-
 func TestExamples(t *testing.T) {
-	ExampleIterator()
+	tests := []func(){
+		ExampleIterator_sql,
+		ExampleIterator_pagination,
+	}
+
+	for i, tt := range tests {
+		tt := tt
+		t.Run(strconv.Itoa(i), func(*testing.T) {
+			tt()
+		})
+	}
 }
